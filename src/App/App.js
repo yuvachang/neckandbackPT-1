03ae5116fa0ab8ff405a76d4b998ce76fb1b25c5
yuvachang <yuva.chang@gmail.com>
2019-07-17 19:51:23 -0400

    file-setup: app, navbar components created

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavBar() {
+  return <div className='navbar'>
+    I am the nav bar.
+  </div>
+}
+
+export default NavBar
